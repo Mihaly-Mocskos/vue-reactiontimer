affc8490fc1817a551d647c26554d0eadd4b8667
Mocskos Mihaly <mocskos.misi@gmail.com>
2021-06-28 11:22:46 +0200

    reaction-timer with vue

---
+++ src/components/Results.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+
+      <p>Your reaction time is {{score}}</p>
+        <h3 class="rank">{{rank}}</h3>
+
+  </div>
+</template>
+
+<script>
+export default {
+props:['score'],
+
+data(){
+    return{
+        rank:null
+    }
+},
+
+mounted(){
+    if (this.score < 250){
+        this.rank = 'Ninja fingers'
+    }else if(this.score<400){
+        this.rank='Rapid reflexes'
+    }else{
+        this.rank='Snail pace..'
+    }
+}
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.4em;
+}
+</style>
